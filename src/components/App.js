87794b57c8fee1147b1b21d89b7c07b0c662d437
hvdson <hvdsonmusic@gmail.com>
2020-12-23 21:25:56 -0800

    refactored using react hooks - displays colours tokens now

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,83 @@
 import React, { useState, useEffect} from 'react';
 import './App.css';
 import Web3 from 'web3';
+import Colour from '../abis/Colour.json'
 
 const App = () => {
-  const [account, setAccount] = useState('');
-  const [tokens, setTokens] = useState([]);
+  const [totalSupply, setTotalSupply] = useState(0);
+  const web3 = new Web3('ws://localhost:7545');
   
   useEffect(() => {
-    const web3 = new Web3('ws://localhost:7545');
-    async function loadWeb3AndGetAccount() {
-      await loadWeb3();
-      await loadBlockchainData();
+    loadWeb3ForMetamask();
+  },[window.ethereum]);
+
+  async function loadWeb3ForMetamask() {
+    // @dev breaking change - no more web3.window injection
+    if (window.ethereum) {
+      await window.ethereum.request({method: "eth_requestAccounts"})
+      .catch(err => console.error(err))
+    } else {
+      window.alert("Non ETH browser detected - try MetaMask!")
     }
+  }
+
+  const [account, setAccount] = useState('');
+  useEffect(() => {
+    getAccountAndSetState();
+  },[])
+
+  async function getAccountAndSetState() {
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+  }
+
+  function checkIfDataExists(data) {
+     return data !== null && data !== undefined;
+  }
+
+  const [contract, setContract] = useState(null);
+  useEffect(() => {
+    createContractFromNetworkData();
+  }, [account])
+
+  async function createContractFromNetworkData() {
+    const networkId = await web3.eth.net.getId();
+    const networkData = Colour.networks[networkId]
+    if (checkIfDataExists(networkData)) {
+      const address = networkData.address;
+      const abi = Colour.abi;
+      const newContract = await new web3.eth.Contract(abi, address);
+      setContract(newContract)
+    }
+  }
+
   
-    // @dev breaking change - no more window.web3
-    async function loadWeb3() {
-      if (window.ethereum) {
-        await window.ethereum.request({method: "eth_requestAccounts"})
-        .catch(err => console.error(err))
-      } else {
-        window.alert("Non ETH browser detected - try MetaMask!")
-      }
+  const [colours, setColours] = useState([]);
+  useEffect(() => {
+    if (contract) {
+      loadColourData();
     }
-
-    async function loadBlockchainData() {
-      const accounts = await web3.eth.getAccounts();
-      setAccount(accounts[0]);
-
+  }, [contract])
+  
+  async function loadColourData() {
+    // part of ERC721
+    const totalSupply = await contract.methods.totalSupply().call();
+    setTotalSupply(totalSupply);
+    let colourState = [];
+    for (let i = 0; i < totalSupply; i++) {
+      let colour = await contract.methods.colours(i).call();
+      colourState.push(colour);
     }
-    loadWeb3AndGetAccount();
-    console.log(account);
-  }, [account]);
+    setColours([...colourState]);
+  }
+
+  // it works!
+  // todo: refactor and clean up
+  const mapColours = () => {
+    return colours.map((item) => {
+      return <span>{item}</span>
+    })
+  }
 
   return (
     <div>
@@ -53,8 +100,11 @@ const App = () => {
           <main role="main" className="col-lg-12 d-flex text-center">
             <div className="content mr-auto ml-auto">
               <h1>yooo this some NFT shit</h1>
+              <hr/>
+              <button className="btn btn-primary" onClick={loadWeb3ForMetamask}>Connect</button>
             </div>
           </main>
+          {mapColours()}
         </div>
       </div>
     </div>
